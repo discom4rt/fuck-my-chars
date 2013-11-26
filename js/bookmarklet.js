11a936fb5a6d1bded73914cb53062942abdf4be3
Morgan Brown <brown.mhg@gmail.com>
2013-11-26 17:43:08 +0100

    Flesh out bookmarklet experimentally

--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -3,15 +3,35 @@
  **/
 (function( window, document, undefined ) {
 
-  var stag;
+  var loading = false,
+   ctag;
 
   if( !window.FuckMyChars ) {
-    stag = document.createElement( 'script' );
-    stag.setAttribute( 'src', 'https://rawgithub.com/discom4rt/fuck-my-chars/master/js/fuck-my-chars.js' );
-    stag.onload = function(){ FuckMyChars.up(); };
-    document.body.appendChild( stag );
+
+    if( loading ) {
+      return;
+    }
+
+    loading = true;
+    ctag = document.createElement( 'link' );
+    ctag.setAttribute('rel', 'sylesheet');
+    ctag.setAttribute('href', 'https://rawgithub.com/discom4rt/fuck-my-chars/master/css/fuck-my-chars.css');
+    ctag.onload = function() {
+      loadScript();
+    };
+    document.body.appendChild( ctag );
   } else {
     FuckMyChars.up();
+  }
+
+  function loadScript() {
+    var stag = document.createElement( 'script' );
+    stag.setAttribute( 'src', 'https://rawgithub.com/discom4rt/fuck-my-chars/master/js/fuck-my-chars.js' );
+    stag.onload = function() {
+      loading = false;
+      FuckMyChars.up();
+    };
+    document.body.appendChild( stag );
   }
 
 }( window, document ));
